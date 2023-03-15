94314a0e4af20a9aa9263287e8597766b2439bd3
RudixMasha <116712267+RudixMasha@users.noreply.github.com>
2023-03-15 18:49:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int lefbord = 0;
+  int rigbord = size - 1;
+  int schet = 0;
+  int sered = 0;
+  while (rigbord >= lefbord) {
+    sered = (rigbord + lefbord) / 2;
+    if (arr[sered] == value) {
+      schet++;
+      int ser = sered - 1;
+      while (arr[sered + 1] == value) {
+        schet++;
+        ser++;
+      }
+      while (arr[ser - 1] == value) {
+        schet++;
+        ser--;
+      }
+      return sered;
+    } else if (value < arr[sered]) {
+      right = sered - 1;
+    } else {
+      left = sered + 1
+    }
+  }
+  return schet;
 }
